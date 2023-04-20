52cc2c2a9ce8a5b964a680ba0d0c257f2ff94b25
Bohdan Kurach <kurach.link@gmail.com>
2023-04-20 03:11:23 +0300

    added the necessary folders

--- src/container/App/App.tsx
+++ src/container/App/App.tsx
@@ -1,21 +1,19 @@
-import { CssBaseline, StyledEngineProvider } from '@mui/material'
+import { StyledEngineProvider } from '@mui/material/styles'
+import CssBaseline from '@mui/material/CssBaseline'
 import Footer from 'container/Footer/Footer'
 import Header from 'container/Header/Header'
 import Main from 'container/Main/Main'
-import React from 'react'
 
 type Props = {}
 
 const App = (props: Props) => {
     return (
-        <React.Fragment>
-            <StyledEngineProvider injectFirst>
-                <CssBaseline />
-                <Header />
-                <Main />
-                <Footer />
-            </StyledEngineProvider>
-        </React.Fragment>
+        <StyledEngineProvider injectFirst>
+            <CssBaseline />
+            <Header />
+            <Main />
+            <Footer />
+        </StyledEngineProvider>
     )
 }
 
