655110862acb304fd5132e3f9f6c97bd20535bbe
Bohdan Kurach <kurach.link@gmail.com>
2023-04-27 03:39:50 +0300

    added arrow-down button

--- src/components/FirstEffect/FirstEffect.tsx
+++ src/components/FirstEffect/FirstEffect.tsx
@@ -1,11 +1,19 @@
+import { Button } from '@mui/material'
 import './FirstEffect.scss'
 
 type Props = {}
 
 const FirstEffect = (props: Props) => {
     return (
-        <div className="background-container">
+        <div
+            aria-label="Кнопка зі стрілкою вниз"
+            className="background-container"
+        >
             <div className="logo_container"></div>
+            <Button variant="text" className="btn-down-arrow">
+                <div className="arrow-down-1"></div>
+                <div className="arrow-down-2"></div>
+            </Button>
         </div>
     )
 }
