d3de49da17701dfd759400b4c5a1ee32bd10fa96
Bohdan Kurach <kurach.link@gmail.com>
2023-05-03 22:21:23 +0300

    .

---
+++ src/components/CartTotal/CartTotal.tsx
@@ -0,0 +1,31 @@
+import productsArray, { Product, getProductObject } from "utils/productsArray"
+
+type Props = {
+    productsInCart: {
+        [id: number]: number
+    }
+    productsObject?: {
+        [id: number]: Product
+    }
+}
+
+const CartTotal = ({
+    productsInCart,
+    productsObject = getProductObject(productsArray),
+}: Props) => {
+    return (
+        <div className="total_price_in_cart">
+            Загальна вартість:{' '}
+            {Object.keys(productsInCart).reduce(
+                (total, productId) =>
+                    total +
+                    productsObject[parseInt(productId)].price *
+                        productsInCart[parseInt(productId)],
+                0
+            )}{' '}
+            грн.
+        </div>
+    )
+}
+
+export default CartTotal
--- src/pages/CartPage/CartPage.tsx
+++ src/pages/CartPage/CartPage.tsx
@@ -1,41 +1,23 @@
-import productsArray, { Product, getProductObject } from 'utils/productsArray'
+// import productsArray, { Product, getProductObject } from 'utils/productsArray'
 import CartPageItem from 'components/CartPageItem/CartPageItem'
 import ScrollToTopOnMount from 'utils/ScrollToTopOnMount'
 import { Container } from '@mui/material'
 import './CartPage.scss'
+import CartTotal from 'components/CartTotal/CartTotal'
 
 type Props = {
     productsInCart: {
         [id: number]: number
     }
-    productsObject?: {
-        [id: number]: Product
-    }
 }
 
-const CartPage = ({
-    productsInCart,
-    productsObject = getProductObject(productsArray),
-}: Props) => {
+const CartPage = ({ productsInCart }: Props) => {
     return (
         <div>
             <ScrollToTopOnMount />
             <Container maxWidth="lg" sx={{ padding: '21px 0px' }}>
-                <CartPageItem
-                    productsInCart={productsInCart}
-                    productsObject={productsObject}
-                />
-                <div className="total_price_in_cart">
-                    Загальна вартість:{' '}
-                    {Object.keys(productsInCart).reduce(
-                        (total, productId) =>
-                            total +
-                            productsObject[parseInt(productId)].price *
-                                productsInCart[parseInt(productId)],
-                        0
-                    )}{' '}
-                    грн.
-                </div>
+                <CartPageItem productsInCart={productsInCart} />
+                <CartTotal productsInCart={productsInCart} />
             </Container>
         </div>
     )
