77f77a6101a37e8b9166e9e27e9ef9d518664f46
Bohdan Kurach <kurach.link@gmail.com>
2023-05-08 14:50:34 +0300

    added NavLink to AlternativeLogo in Header

--- src/container/Header/Header.tsx
+++ src/container/Header/Header.tsx
@@ -3,6 +3,7 @@ import Toolbar from '@mui/material/Toolbar'
 import AlternativeLogo from 'components/AlternativeLogo/AlternativeLogo'
 import Container from '@mui/material/Container/Container'
 import NavMenu from 'components/NavMenu/NavMenu'
+import { NavLink } from 'react-router-dom'
 
 type Props = {
     cartData: {
@@ -25,11 +26,13 @@ const Header = ({ cartData }: Props) => {
             }}
         >
             <Container maxWidth="xl">
-                <Toolbar>
-                    <AlternativeLogo />
-                    <NavMenu
-                        cartData={cartData}
-                    />
+                <Toolbar
+                    sx={{ display: 'flex', justifyContent: 'space-between' }}
+                >
+                    <NavLink to="/">
+                        <AlternativeLogo />
+                    </NavLink>
+                    <NavMenu cartData={cartData} />
                 </Toolbar>
             </Container>
         </AppBar>
