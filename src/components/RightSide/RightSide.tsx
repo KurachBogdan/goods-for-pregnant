7d7ed629cfae54a24600766def9d23817b878fb8
Bohdan Kurach <kurach.link@gmail.com>
2023-05-10 03:58:14 +0300

    added ready RightSide

--- src/components/RightSide/RightSide.tsx
+++ src/components/RightSide/RightSide.tsx
@@ -1,5 +1,6 @@
 import articlesArray from 'utils/articlesArray'
 import './RightSide.scss'
+import { CardMedia } from '@mui/material'
 
 type Props = {
     rightSideLogo: string
@@ -9,13 +10,28 @@ type Props = {
 const RightSide = ({ rightSideLogo, rightSideTitle }: Props) => {
     return (
         <div className="right_side-container">
-            <div className="right_side-title">
+            <div className="title_container">
                 <p className="right_side-logo">{rightSideLogo}</p>
                 <p className="right_side-title">{rightSideTitle}</p>
             </div>
-
+            {articlesArray.map(
+                ({
+                    informativeTitle,
+                    informativeImage,
+                    informativeArticle,
+                }) => (
+                    <div className="article_container">
+                        <CardMedia  sx={{ width: '77px', height: '154px' }} image={informativeImage}/>
+                        <div className="article_part">
+                            <p className="article_title">{informativeTitle}</p>
+                            <p className="right_side-article">{informativeArticle}</p>
+                        </div>
+                    </div>
+                )
+            )}
         </div>
     )
 }
 
 export default RightSide
+// sx={{ height: 232 }} image={image}
