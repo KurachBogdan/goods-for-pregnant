5ebc7a19d12050720576fe9c9302128f3253ea75
Bohdan Kurach <kurach.link@gmail.com>
2023-05-11 19:35:57 +0300

    added ScrollTop to Home

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -4,13 +4,52 @@ import RightSide from 'components/RightSide/RightSide'
 import './Home.scss'
 import FirstEffect from 'components/FirstEffect/FirstEffect'
 import ScrollToTopOnMount from 'utils/ScrollToTopOnMount'
+import Fab from '@mui/material/Fab'
+import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp'
+import { Box, Fade, Typography, useScrollTrigger } from '@mui/material'
 
-type Props = {}
+type Props = {
+    window?: () => Window
+    children?: React.ReactElement
+}
+
+function ScrollTop(props: Props) {
+    const { children, window } = props
+    const trigger = useScrollTrigger({
+        target: window ? window() : undefined,
+        disableHysteresis: true,
+        threshold: 150,
+    })
+
+    const handleClick = (event: React.MouseEvent<HTMLDivElement>) => {
+        const anchor = (
+            (event.target as HTMLDivElement).ownerDocument || document
+        ).querySelector('#back-to-top-anchor')
+
+        if (anchor) {
+            anchor.scrollIntoView({
+                block: 'center',
+            })
+        }
+    }
+    return (
+        <Fade in={trigger}>
+            <Box
+                onClick={handleClick}
+                role="presentation"
+                sx={{ position: 'fixed', bottom: 16, right: 16, zIndex: '1' }}
+            >
+                {children}
+            </Box>
+        </Fade>
+    )
+}
 
 const Home = (props: Props) => {
     return (
         <>
             <ScrollToTopOnMount />
+            <Typography id="back-to-top-anchor" component="div" />
             <FirstEffect />
             <div className="home_container">
                 <Container
@@ -28,6 +67,11 @@ const Home = (props: Props) => {
                     />
                 </Container>
             </div>
+            <ScrollTop {...props}>
+                <Fab size="small" aria-label="scroll back to top">
+                    <KeyboardArrowUpIcon />
+                </Fab>
+            </ScrollTop>
         </>
     )
 }
