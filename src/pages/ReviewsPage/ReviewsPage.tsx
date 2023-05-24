5d99c1cfc6382a5f8f2267c505ce5ed9da144eef
Bohdan Kurach <kurach.link@gmail.com>
2023-05-24 22:54:29 +0300

    added some reviews to ReviewsPage with ability to add new review

--- src/pages/ReviewsPage/ReviewsPage.tsx
+++ src/pages/ReviewsPage/ReviewsPage.tsx
@@ -1,11 +1,82 @@
-import { Container, Typography } from '@mui/material'
+import { TextField, TextareaAutosize } from '@mui/material'
+import { Button, Card, CardContent, Container } from '@mui/material'
 import BackToCategoriesBtn from 'components/BackToCategoriesBtn/BackToCategoriesBtn'
 import EveryPageTitle from 'components/EveryPageTitle/EveryPageTitle'
+import { useState } from 'react'
 import ScrollToTopOnMount from 'utils/ScrollToTopOnMount'
+import './ReviewsPage.scss'
 
 type Props = {}
 
+type Review = {
+    name: string
+    text: string
+    email?: string
+}
+
 const ReviewsPage = (props: Props) => {
+    const arrReviews: Review[] = [
+        {
+            name: 'Ольга Б.',
+            text: 'Подушка дуже чудова і практична. Дитина на ній солодко спить. Та і я теж полюбляю на ній лежати) Дуже задоволена і, звісно ж, рекомендую!',
+        },
+        {
+            name: 'Олеся',
+            text: 'Дитина в захваті. Чудова іграшка!',
+        },
+        {
+            name: 'Яна К.',
+            text: "Дуже м'який пледик, на зиму те що треба",
+        },
+        {
+            name: 'Марія',
+            text: 'Круті патріотичні бодіки. Я задоволена.',
+        },
+    ]
+
+    const [reviews, setReviews] = useState<Review[]>(arrReviews)
+    const [newReview, setNewReview] = useState<Review>({
+        name: '',
+        text: '',
+    })
+
+    const handleName = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setNewReview((prevState) => ({
+            ...prevState,
+            name: e.target.value,
+        }))
+    }
+
+    const handleText = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+        setNewReview((prevState) => ({
+            ...prevState,
+            text: e.target.value,
+        }))
+    }
+
+    const handleEmail = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+        setNewReview((prevState) => ({
+            ...prevState,
+            email: e.target.value,
+        }))
+    }
+
+    const onSend = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault()
+        if (newReview.name === '' || newReview.text === '') {
+            alert("Вкажіть Ваше ім'я та сам коментар")
+        } else {
+            setNewReview({
+                name: '',
+                email: '',
+                text: '',
+            })
+
+            setReviews((prevState) => {
+                return [...prevState, newReview]
+            })
+        }
+    }
     return (
         <Container
             sx={{ padding: '21px 24px', minHeight: 'calc(100vh - 430px)' }}
@@ -13,8 +84,73 @@ const ReviewsPage = (props: Props) => {
         >
             <ScrollToTopOnMount />
             <EveryPageTitle title="Відгуки" />
+            <div className="reviews">
+                <div>
+                    {reviews.map((item, i) => (
+                        <Card
+                            className="comment-card"
+                            variant="outlined"
+                            sx={{ margin: '7px 0' }}
+                            key={i}
+                        >
+                            <CardContent>
+                                <strong>{item.name}:</strong>
+                                <div>{item.email}</div>
+                                <div>{item.text}</div>
+                            </CardContent>
+                        </Card>
+                    ))}
+                </div>
+                <form onSubmit={onSend}>
+                    <h3
+                        style={{
+                            fontFamily: `'Exo 2', sans-serif`,
+                            fontSize: '21px',
+                            color: '#105b63',
+                        }}
+                    >
+                        Будь ласка, залиште відгук
+                    </h3>
+                    <div>
+                        <TextField
+                            className="form_name_field"
+                            label="Ім'я"
+                            value={newReview.name}
+                            onChange={handleName}
+                        />
+                    </div>
+                    <br />
+                    <div>
+                        <TextField
+                            className="form_name_field"
+                            label="E-mail"
+                            value={newReview.email}
+                            onChange={handleEmail}
+                        />
+                    </div>
+                    <br />
+                    <div>
+                        <TextareaAutosize
+                            className="form_text_field"
+                            minRows={7}
+                            placeholder="Текст"
+                            value={newReview.text}
+                            onChange={handleText}
+                        />
+                    </div>
+                    <div style={{ textAlign: 'center', marginTop: '14px' }}>
+                        <Button
+                            className="form_btn"
+                            type="submit"
+                            variant="outlined"
+                        >
+                            Відправити
+                        </Button>
+                    </div>
+                </form>
+            </div>
             <div>
-                <Typography
+                {/* <Typography
                     sx={{
                         textTransform: 'none',
                         fontFamily: `'Exo 2', sans-serif`,
@@ -27,7 +163,7 @@ const ReviewsPage = (props: Props) => {
                     component="div"
                 >
                     Ще немає відгуків
-                </Typography>
+                </Typography> */}
                 <BackToCategoriesBtn />
             </div>
         </Container>
