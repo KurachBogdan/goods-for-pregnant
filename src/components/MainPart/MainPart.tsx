d42130d6925a5f4c2c1946f6d72d4e29d6bfec77
Bohdan Kurach <kurach.link@gmail.com>
2023-06-29 00:41:15 +0300

    .

--- src/components/MainPart/MainPart.tsx
+++ src/components/MainPart/MainPart.tsx
@@ -19,7 +19,8 @@ const MainPart = (props: Props) => {
                     fontSize: '35px',
                     lineHeight: '35px',
                     fontWeight: 600,
-                    color: '#105b63',
+                    // color: '#105b63',
+                    color: 'red',
                     cursor: 'context-menu',
                     margin: '14px 0px 0px 0px'
                 }}
