3f60e2505397a78a852baac6c1c04a3fa125ddca
Bohdan Kurach <kurach.link@gmail.com>
2023-07-18 03:53:14 +0300

    test

--- src/pages/ReviewsPage/ReviewsPage.tsx
+++ src/pages/ReviewsPage/ReviewsPage.tsx
@@ -263,7 +263,7 @@ const ReviewsPage: React.FC = () => {
                         </Button>
                         <Snackbar
                             open={openSnack}
-                            autoHideDuration={1000}
+                            autoHideDuration={2000}
                             onClose={handleCloseSnack}
                             anchorOrigin={{
                                 vertical: 'top',
